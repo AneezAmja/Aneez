d4798e61e2446e7cfdc7764676a8d6742fbf4111
AneezAmja <aneez.amja@gmail.com>
2024-03-22 23:53:52 +0000

    added experience component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from './components/Navbar';
 import Home from "./components/Home";
 import About from "./components/About";
-
+import Experience from './components/Experience';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Experience />
     </div>
   );
 }
