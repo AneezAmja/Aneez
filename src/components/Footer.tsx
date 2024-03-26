3439c955dfaf86687120c7ab68c536e641f34641
AneezAmja <aneez.amja@gmail.com>
2024-03-26 01:21:57 +0000

    Footer created

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center py-[50px] text-sm">
+      <div className="text-center">Built and designed by Aneez Amja. 
+        <br />
+        All rights reserved. ©</div>
+    </div>
+  );
+};
+
+export default Footer;
