d4295515d37478bd165572f85275288156d8fd09
AneezAmja <aneez.amja@gmail.com>
2024-04-29 20:26:25 +0100

    updated height for content to be dynamic

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,7 +5,7 @@ const Experience = () => {
 
 
   return (
-    <div className="h-screen md:h-4/5 container mx-auto max-w-[1000px] px-8">
+    <div className="h-max-content md:h-4/5 container mx-auto max-w-[1000px] px-8">
     <h1 className="text-3xl md:text-[45px] text-secondary-content font-bold md:py-5 relative">
       / experience
       <div className="absolute after:inline-block after:bg-[#233554] after:h-[1px] after:w-300 after:text-#f6cccc after:ml-[70px]"></div>
