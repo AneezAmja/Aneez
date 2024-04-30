9a73adb1b267048ef124104dbea8e2b03c7b55fd
AneezAmja <aneez.amja@gmail.com>
2024-04-30 20:54:42 +0100

    adding in animation, rm redudant code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { FaAngleLeft, FaAngleRight, FaGithub, FaUpload } from "react-icons/fa";
 import useIsMobileView from "../util/useIsMobileView";
+import FadeIn from "./FadeIn";
 
 const projects = {
   ecom: {
@@ -10,6 +11,8 @@ const projects = {
     link: "https://github.com/AneezAmja/ecommerce-project",
     open: "https://ecommerce-project-bay.vercel.app/products",
     image: "/src/assets/performance-plus.png",
+    githubLink: "https://github.com/AneezAmja/ecommerce-project",
+    liveLink: "https://ecommerce-project-bay.vercel.app/products",
   },
   spotify: {
     title: "All the time wrapped",
@@ -18,6 +21,8 @@ const projects = {
     link: "https://github.com/AneezAmja/ecommerce-project",
     open: "https://ecommerce-project-bay.vercel.app/products",
     image: "/src/assets/spotify.png",
+    githubLink: null,
+    liveLink: null,
   },
   aneez_notes: {
     title: "Notes by Aneez",
@@ -26,6 +31,8 @@ const projects = {
     link: "https://github.com/AneezAmja/ecommerce-project",
     open: "https://ecommerce-project-bay.vercel.app/products",
     image: "/src/assets/aneez_notes-2.jpg",
+    githubLink: "https://github.com/AneezAmja/notesbyaneez",
+    liveLink: null,
   },
   pokedex: {
     title: "Pokedex",
@@ -34,6 +41,8 @@ const projects = {
     link: "https://github.com/AneezAmja/ecommerce-project",
     open: "https://ecommerce-project-bay.vercel.app/products",
     image: "/src/assets/pokemon.jpg",
+    githubLink: "https://github.com/AneezAmja/pokedex",
+    liveLink: null,
   },
   coursework: {
     title: "rocketz",
@@ -42,6 +51,8 @@ const projects = {
     link: "https://github.com/AneezAmja/ecommerce-project",
     open: "https://ecommerce-project-bay.vercel.app/products",
     image: "/src/assets/spotify.png",
+    githubLink: null,
+    liveLink: null,
   },
 };
 
@@ -71,107 +82,125 @@ const Projects = () => {
           (prevProj) => (prevProj + 1) % Object.keys(projects).length,
         );
       }, 8000);
-  
+
       return () => {
         clearInterval(interval);
       };
     }
   }, [isMobileView]);
-  
+
   if (isMobileView) {
     return (
       <div className="h-4/5 container px-8">
-
-      <h1 className="text-3xl  text-secondary-content font-bold py-5 relative">
-      / projects
-    </h1>
-      <div className="flex flex-col items-center pb-8"> 
-        {Object.keys(projects).map((key, i) => (
-          <div key={i} className="mb-12"> 
-            <img src={projects[key].image} alt="" className="w-full rounded-lg" />
-            <h2 className="text-center my-3 text-[#fefefe] text-xl lowercase font-bold">{projects[key].title}</h2> 
-            <p className="text-center text-[#fefefe] text-sm font-medium">{projects[key].desc}</p>
-            <p className="text-center text-secondary-content text-sm font-bold pt-3">{projects[key].techStack}</p>
-          </div>
-        ))}
-
-      </div>
-      </div>
-    );
-  }
-  
-
-  return (
-    <div className="h-full container mx-auto max-w-[1000px] px-8">
-      <h1 className="text-3xl md:text-[45px] text-secondary-content font-bold md:py-5 relative">
-        / projects
-      </h1>
-
-      <div className="h-[500px] overflow-hidden relative rounded-2xl mt-6">
-        <div className={`${isMobileView ? "absolute inset-0 flex" : "grid grid-cols-5"}`}>
+        <FadeIn>
+          <h1 className="text-3xl  text-secondary-content font-bold py-5 relative">
+            / projects
+          </h1>
+        </FadeIn>
+        <div className="flex flex-col items-center pb-8">
           {Object.keys(projects).map((key, i) => (
-            <div
-              key={i}
-              className={`w-full h-full ${
-                selectedProject === i ? "opacity-100" : "opacity-0"
-              } absolute top-0 left-0 transition-opacity duration-500`}
-            >
-              <div className="relative h-full">
-                <div className="absolute inset-0"></div>
+            <FadeIn>
+              <div key={i} className="mb-12">
                 <img
                   src={projects[key].image}
                   alt=""
-                  className="w-full h-full"
+                  className="w-full rounded-lg"
                 />
+                <h2 className="text-center my-3 text-[#fefefe] text-xl lowercase font-bold">
+                  {projects[key].title}
+                </h2>
+                <p className="text-center text-[#fefefe] text-sm font-medium">
+                  {projects[key].desc}
+                </p>
+                <p className="text-center text-secondary-content text-sm font-bold pt-3">
+                  {projects[key].techStack}
+                </p>
+              </div>
+            </FadeIn>
+          ))}
+        </div>
+      </div>
+    );
+  }
 
-                <div className="absolute bottom-0 left-0 p-4 w-full bg-gradient-to-t from-black to-transparent">
-                  <div className="text-center w-1/2 m-auto">
-                    <h1 className="text-[#fefefe] text-3xl lowercase font-bold mb-5 text-stroke-3">
-                      {projects[key].title}
-                    </h1>
-                    <p className="text-[#fefefe] text-sm font-medium [text-shadow:_0_1px_0_rgb(0_0_0_/_100%)]">
-                      {projects[key].desc}
-                    </p>
-                    <p className="text-secondary-content text-sm font-bold mt-2">
-                      {projects[key].techStack}
-                    </p>
+  return (
+    <div className="h-full container mx-auto max-w-[1000px] px-8">
+      <FadeIn>
+        <h1 className="text-3xl md:text-[45px] text-secondary-content font-bold md:py-5 relative">
+          / projects
+        </h1>
+      </FadeIn>
+
+      <FadeIn>
+        <div className="h-[500px] overflow-hidden relative rounded-2xl mt-6">
+          <div className="absolute inset-0 flex">
+            {Object.keys(projects).map((key, i) => (
+              <div
+                key={i}
+                className={`w-full h-full ${
+                  selectedProject === i ? "opacity-100" : "opacity-0"
+                } absolute top-0 left-0 transition-opacity duration-500`}
+              >
+                <div className="relative h-full">
+                  <div className="absolute inset-0"></div>
+                  <img
+                    src={projects[key].image}
+                    alt=""
+                    className="w-full h-full"
+                  />
+
+                  <div className="absolute bottom-0 left-0 p-4 w-full bg-gradient-to-t from-black to-transparent">
+                    <div className="text-center w-1/2 m-auto">
+                      <h1 className="text-[#fefefe] text-3xl lowercase font-bold mb-5 text-stroke-3">
+                        {projects[key].title}
+                      </h1>
+                      <p className="text-[#fefefe] text-sm font-medium [text-shadow:_0_1px_0_rgb(0_0_0_/_100%)]">
+                        {projects[key].desc}
+                      </p>
+                      <p className="text-secondary-content text-sm font-bold mt-2">
+                        {projects[key].techStack}
+                      </p>
+                      <div className="text-[#fefefe] mt-2 flex justify-center">
+                        {projects[key].githubLink && (
+                          <a
+                            className="pointer pr-6 z-50"
+                            href={projects[key].githubLink}
+                          >
+                            <FaGithub />
+                          </a>
+                        )}
+                        {projects[key].liveLink && (
+                          <a
+                            className="pointer z-50"
+                            href={projects[key].liveLink}
+                          >
+                            <FaUpload />
+                          </a>
+                        )}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-    {!isMobileView && (
-      <>
-      <button
-            className="absolute top-1/2 left-0 transform -translate-y-1/2  p-2 text-secondary-content text-5xl"
-            onClick={goToPreviousImage}
-          >
-            <FaAngleLeft />
-          </button>
-          <button
-            className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 text-secondary-content text-5xl"
-            onClick={goToNextImage}
-          >
-            <FaAngleRight />
-          </button>
-      </>
-      )}
-          {/* 
-          <button
-            className="absolute top-1/2 left-0 transform -translate-y-1/2  p-2 text-secondary-content text-5xl"
-            onClick={goToPreviousImage}
-          >
-            <FaAngleLeft />
-          </button>
-          <button
-            className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 text-secondary-content text-5xl"
-            onClick={goToNextImage}
-          >
-            <FaAngleRight />
-          </button> 
-          */}
+            ))}
+
+            <>
+              <button
+                className="absolute top-1/2 left-0 transform -translate-y-1/2  p-2 text-secondary-content text-5xl"
+                onClick={goToPreviousImage}
+              >
+                <FaAngleLeft />
+              </button>
+              <button
+                className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 text-secondary-content text-5xl"
+                onClick={goToNextImage}
+              >
+                <FaAngleRight />
+              </button>
+            </>
+          </div>
         </div>
-      </div>
+      </FadeIn>
     </div>
   );
 };
